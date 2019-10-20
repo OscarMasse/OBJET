66968c1d83c126c91da4c29793d751db1f5e60ea
NakedSyned <synedday@gmail.com>
2019-10-20 17:21:43 +0200

    Deleting Old Frame and GameView, Modifying GameMain to use LWJGL

---
+++ src/pro/masse/main/Display.java
@@ -0,0 +1,128 @@
+package pro.masse.main;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.glfw.GLFW.*;
+
+public class Display {
+
+    private final long id;
+    private GameMain gameMain;
+    private IntBuffer widthBuffer, heightBuffer;
+    private int width, height;
+
+    /**
+     * This error callback will simply print the error to
+     * <code>System.err</code>.
+     */
+    private static GLFWErrorCallback errorCallback
+            = GLFWErrorCallback.createPrint(System.err);
+
+    /**
+     * This key callback will check if ESC is pressed and will close the window
+     * if it is pressed.
+     */
+    private static GLFWKeyCallback keyCallback = new GLFWKeyCallback() {
+
+        @Override
+        public void invoke(long window, int key, int scancode, int action, int mods) {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+                glfwSetWindowShouldClose(window, true);
+            }
+        }
+    };
+
+    public Display(GameMain gameMain, int width, int height, String title) {
+
+        /* Set the error callback */
+        glfwSetErrorCallback(errorCallback);
+
+        /* Initialize GLFW */
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        /* Make window non-resizable */
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        /* Create window */
+        id = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (id == NULL) {
+            glfwTerminate();
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        /* Center window on screen */
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(id,
+                (vidmode.width() - width) / 2,
+                (vidmode.height() - height) / 2
+        );
+
+        /* Create OpenGL context */
+        glfwMakeContextCurrent(id);
+        GL.createCapabilities();
+
+//        /* Enable vertical synchronization */
+//        glfwSwapInterval(1);
+
+        /* Set the key callback */
+        glfwSetKeyCallback(id, keyCallback);
+
+        widthBuffer = MemoryUtil.memAllocInt(1);
+        heightBuffer = MemoryUtil.memAllocInt(1);
+
+        this.gameMain = gameMain;
+    }
+
+    public void update() {
+        glfwGetFramebufferSize(id, widthBuffer, heightBuffer);
+
+        // Reset buffer pos
+        widthBuffer.rewind();
+        heightBuffer.rewind();
+
+        width = widthBuffer.get();
+        height = heightBuffer.get();
+
+        /* Swap buffers and poll Events */
+        glfwSwapBuffers(id);
+        glfwPollEvents();
+
+        widthBuffer.flip();
+        heightBuffer.flip();
+
+        if(glfwWindowShouldClose(id)) {
+            gameMain.stop();
+        }
+    }
+
+    public void dispose() {
+        /* Free buffers */
+        MemoryUtil.memFree(widthBuffer);
+        MemoryUtil.memFree(heightBuffer);
+
+        /* Release window and its callbacks */
+        glfwDestroyWindow(id);
+        keyCallback.free();
+
+        /* Terminate GLFW and release the error callback */
+        glfwTerminate();
+        errorCallback.free();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
--- src/pro/masse/main/GameMain.java
+++ src/pro/masse/main/GameMain.java
@@ -1,27 +1,44 @@
 package pro.masse.main;
 
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL11;
+
 public class GameMain {
 
-    public static Frame window;
+    private GLFWErrorCallback errorCallback;
+
+    private boolean running;
+    private Display display;
+    private Timer timer;
+    private Scene loadedScene;
+    private Graphics2D graphics2D;
+
     public static int fps;
 
-    private static boolean running;
-
-    public static void start() {
-        window = new Frame();
+    public void start() {
         if (running) return;
         running = true;
+
+        display = new Display(this, 1600, 900, "Game Title");
+        loadedScene = new Scene();
+
+        ///////////////////////////////////////
+        loadedScene.addEntity(new Player());
+        graphics2D = new Graphics2D();
+        ///////////////////////////////////////
+
+        gameLoop();
     }
 
-    private static void update(double deltaTime) {
+    private void update(double deltaTime) {
 
     }
 
-    private static void render(double deltaTime) {
+    private void render(double deltaTime) {
 
     }
 
-    public static void gameLoop() {
+    public void gameLoop() {
 
         int targetFPS = 144;
         int lastFpsTime = 0;
@@ -29,6 +46,9 @@ public class GameMain {
         double nanoSecondPerTick = Math.pow(10,9) / targetFPS;
 
         while (running) {
+
+
+
             long now = System.nanoTime();
             long updateLength = now - lastLoopTime;
             lastLoopTime = now;
@@ -40,32 +60,36 @@ public class GameMain {
 
             if (lastFpsTime >= Math.pow(10,9)) {
                 System.out.println("FPS: " + fps);
-                GameView.fps = fps;
                 lastFpsTime = 0;
                 fps = 0;
             }
 
-            update(deltaTime);
+            display.update();
 
-            render(deltaTime);
+            /* Set viewport and clear screen */
+            GL11.glViewport(0, 0, display.getWidth(), display.getHeight());
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 
-            try {
-                Thread.sleep((long) (Math.abs((lastLoopTime - System.nanoTime()) + nanoSecondPerTick)/Math.pow(10,9)));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            /* Set ortographic projection */
+            GL11.glMatrixMode(GL11.GL_PROJECTION);
+            GL11.glLoadIdentity();
+            GL11.glOrtho(0f, 400f, 225f, 0f, 1f, -1f);
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+            loadedScene.update();
+            loadedScene.render(graphics2D);
         }
     }
 
-    public static void stop() {
+    public void stop() {
         if (!running) return;
         running = false;
     }
 
     public static void main(String[] args) {
 
-        start();
+        GameMain gameMain = new GameMain();
 
-        gameLoop();
+        gameMain.start();
     }
 }
